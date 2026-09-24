b6228467a1608bfe3951a778bffe99d9a5be3683
Matthew <42matsss@gmail.com>
2026-09-24 16:17:09 +0800

    express webroot for supporting files - 3

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(cookieParser());
 
 app.use(express.static("Dromic"));
 app.use(express.static("supportFiles"));
+app.use(express.static("Images"));
 app.get("/", (req, res) => {
   res.redirect("/dromic-login.html");
 });
